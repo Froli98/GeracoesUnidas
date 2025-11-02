366cfb99a46c1c366847366e3608218dada249eb
Froli98 <froli98@outlook.com>
2025-11-02 11:03:05 +0000

    Updated supabase.ts

--- project/lib/supabase.ts
+++ project/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/database';
 
-const supabaseUrl = 'https://0ec90b57d6e95fcbda19832f.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwODA5MDAsImV4cCI6MTc5MzYxNjkwMH0.fqPjJ_jCpP_IkcOXD9-Etrtc4ycunMb8BYmPV9I1X5A';
+const supabaseUrl = 'https://lwzpjbihfquchflqjewv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3enBqYmloZnF1Y2hmbHFqZXd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMzkyNDUsImV4cCI6MjA3NzYxNTI0NX0.HnmcVbEb_ATjjpwz8FXzcviLL7xJ6ZEqCCK5QYtLpuM';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
